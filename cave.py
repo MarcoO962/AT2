925d8908adb8325d3317c8632b882c8f24aa4157
MarcoO962 <marco.ochotorena962@education.nsw.gov.au>
2025-06-02 09:43:55 +1000

    Update cave.py

--- cave.py
+++ cave.py
@@ -3,8 +3,10 @@ class Cave:
     def __init__(self,  cave_name):
         self.name = cave_name
         self.description = None
+    def set_description(self, cave_description):
+        self.description = cave_description
     def get_description(self):
         return self.description
     def describe(self):
-    print( self.description )
+        print( self.description )
 
