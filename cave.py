1d1ff7af97f8b5e1072812cb453a5da27fdb929e
MarcoO962 <190552521+MarcoO962@users.noreply.github.com>
2025-06-02 13:53:40 +1000

    update

--- cave.py
+++ cave.py
@@ -2,10 +2,21 @@ class Cave:
     def __init__(self,  cave_name):
         self.name = cave_name
         self.description = None
+        self.linked_caves = {}
     def set_description(self, cave_description):
         self.description = cave_description
     def get_description(self):
         return self.description
+    def set_name(self, cave_name):
+        self.name = cave_name
+    def get_name(self):
+        return self.name
     def describe(self):
-        print( self.description )
+        print( self.description)
+    def link_cave(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
+    def get_details(self):
+        for direction in self.linked_caves:
+            cave = self.linked_caves[direction]
+            print( "The " + cave.get_name() + " is " + direction)
 
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from cave import Cave
 
-cavern = Cave("cavern")
-grotto = Cave("grotto")
+cavern = Cave("Cavern")
+cavern.set_description("A dank and dirty cave ")
 dungeon = Cave("Dungeon")
-cavern.set_description("A damp and dirty cave.")
-cavern.describe()
+dungeon.set_description("A large cave with a rack")
+grotto = Cave("Grotto")
+grotto.set_description("A small cave with ancient graffiti")
+cavern.link_cave(dungeon, "south")
+grotto.link_cave(dungeon, "east")
+dungeon.link_cave(grotto, "west")
+
+dungeon.get_details()
 
