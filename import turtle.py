4385e7c7c785c22bbaaa02f4cc91c5a1ae64fe44
MarcoO962 <190552521+MarcoO962@users.noreply.github.com>
2025-06-23 13:09:21 +1000

    Add files via upload

---
+++ import turtle.py
@@ -0,0 +1,17 @@
+import turtle
+import random
+
+minSpeed = 5
+maxSpedd = 20
+
+colors = []
+speeds = []
+pos = []
+Its = []
+
+screen = turtle.Screen()
+screen.setup(width=1000,height=1000)
+screen.bgcolor("black")
+screen.tracer(0)
+
+turtle.forward(110)
