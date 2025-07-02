ac67447ddffcfde3992060bb4788ce280324d82e
MarcoO962 <marcoocram.eightorena@gmail.com>
2025-07-03 06:06:57 +1000

    update

--- Marco Eli Game/test.py
+++ Marco Eli Game/test.py
@@ -1,15 +1,45 @@
 import turtle 
+
 pen =  turtle.Turtle()
 screen = turtle.Screen()
-
-
+screen.screensize(640, 480)
+screen.bgcolor("black")
 pen.speed(0)
 pen.hideturtle()
 pen.penup()
-pen.goto(-250, 250)
-pen.pendown()
-pen.pencolor("green")
+pen.shape("square")
+pen.pensize(5)
+pen.pencolor("lime")
 
-pen.write("I", font=("arial", 24, "bold italic"))
+
+pen.goto(60, -80)
+pen.pendown()
+pen.write("CLICK SCREEN TO WRITE", font=("fixedsys", 20, "bold"))
+pen.penup()
+pen.goto(50, -100)
+pen.pendown()
+pen.forward(400)
+pen.penup()
+
+def activate_textbox():
+    user_text = screen.textinput("Input", "Enter text for display:")
+    pen.clear()
+    pen.goto(50, -100)
+    pen.pendown()
+    pen.forward(400)
+    pen.penup()
+    if user_text is not None:
+        # Clear old text if needed and display new one
+        pen.penup()
+        pen.goto(60, -80)
+        pen.pendown()
+        pen.write(">"+str(user_text.upper()), font=("fixedsys", 20, "bold"))
+        pen.penup()
+    screen.listen()
+
+# Key binding to simulate text box interaction
+screen.listen()
+screen.onkey(activate_textbox, "t")  # Press spacebar to trigger input
+screen.onscreenclick(lambda x, y: activate_textbox())
 
 turtle.done()
--- Marco Eli Game/test2.py
+++ Marco Eli Game/test2.py
@@ -1,10 +1,35 @@
-import tkinter as tk
-from tkinter import font
+import turtle
 
-root = tk.Tk()
-fonts = list(font.families())
-fonts.sort()
+pen =  turtle.Turtle()
+screen = turtle.Screen()
+screen.screensize(640, 480)
+screen.bgcolor("black")
+pen.speed(0)
+pen.hideturtle()
+pen.penup()
+pen.shape("square")
+pen.pensize(5)
+pen.pencolor("lime")
 
-for f in fonts:
-    print(f)
-root.destroy()
+pen.goto(60, -80)
+pen.pendown()
+pen.write("CLICK SCREEN TO WRITE", font=("fixedsys", 20, "bold"))
+pen.penup()
+pen.goto(50, -100)
+pen.pendown()
+pen.forward(400)
+pen.penup()
+
+
+
+
+
+
+
+pen.goto(-450, 350)
+pen.pendown()
+
+
+pen.write("TEST /n ", font=("fixedsys", 30, "bold"))
+
+turtle.done()
