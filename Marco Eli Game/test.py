98f7a8a6f41bfd8b270de9081f844b4ceffe723f
MarcoO962 <marcoocram.eightorena@gmail.com>
2025-07-02 20:12:54 +1000

    update

--- Marco Eli Game/test.py
+++ Marco Eli Game/test.py
@@ -1,12 +1,15 @@
 import turtle 
-
 pen =  turtle.Turtle()
 screen = turtle.Screen()
 
 
+pen.speed(0)
+pen.hideturtle()
+pen.penup()
+pen.goto(-250, 250)
+pen.pendown()
+pen.pencolor("green")
 
-for i in range(4):
-    pen.forward(10)
-    pen.right(90)
+pen.write("I", font=("arial", 24, "bold italic"))
 
 turtle.done()
---
+++ Marco Eli Game/test2.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import font
+
+root = tk.Tk()
+fonts = list(font.families())
+fonts.sort()
+
+for f in fonts:
+    print(f)
+root.destroy()
